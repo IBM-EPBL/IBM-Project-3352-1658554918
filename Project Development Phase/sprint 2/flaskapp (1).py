2c8a25c943d75e8ccbb57c2538453e47183f6660
Arunsnalan <114383736+Arunsnalan@users.noreply.github.com>
2022-11-18 15:37:58 +0530

    Add files via upload

---
+++ Project Development Phase/sprint 2/flaskapp (1).py
@@ -0,0 +1,158 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators, SelectField, IntegerField
+import ibm_db
+from passlib.hash import sha256_crypt
+from functools import wraps
+import win32api
+#creating an app instance
+app = Flask(__name__)
+
+#connecting to ibmdb2
+print("Trying to connect...")
+conn = db.connect("DATABASE=bludb;HOSTNAME=98538591-7217-4024-b027-8baa776ffad1.c3n41cmd0nqnrk39u98g.databases.appdomain.cloud;PORT=30875;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=xwb32247;PWD=XLpYFeILredyL6gF;", '', '')
+print("connected..")
+
+
+#User login
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        #Get form fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        sql1="Select * from users where username = ?"
+        stmt1 = ibm_db.prepare(conn, sql1)
+        ibm_db.bind_param(stmt1,1,username)
+        result=ibm_db.execute(stmt1)
+        d=ibm_db.fetch_assoc(stmt1)
+        if result > 0:
+            #Get hash of the password
+            data = d
+            password = data['PASSWORD']
+
+            #compare passwords
+            if sha256_crypt.verify(password_candidate, password):
+                #Passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash("you are now logged in","success")
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Invalid Login'
+                return render_template('login.html', error=error)
+            #Close connection
+            cur.close()
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error=error)
+    return render_template('login.html')
+    
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        sql1="INSERT INTO users(name, email, password) VALUES(?,?,?)"
+        stmt1 = ibm_db.prepare(conn, sql1)
+        ibm_db.bind_param(stmt1,1,name)
+        ibm_db.bind_param(stmt1,2,email)
+        ibm_db.bind_param(stmt1,3,username)
+        ibm_db.bind_param(stmt1,4,password)
+        ibm_db.execute(stmt1)
+        #for flash messages taking parameter and the category of message to be flashed
+        flash("You are now registered and can log in", "success")
+        
+#check if user logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login','danger')
+            return redirect(url_for('login'))
+    return wrap
+
+#Logout
+@app.route('/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    flash("You are now logged out", "success")
+    return redirect(url_for('login'))
+
+@app.route('/dashboard')
+@is_logged_in
+def homescreen():
+    return render_template('homescreen.html')
+
+def changethreshold(value):
+    sql1="INSERT INTO stockconfig(threshold) VALUES(?)"
+        stmt1 = ibm_db.prepare(conn, sql1)
+        ibm_db.bind_param(stmt1,1,value)
+        ibm_db.execute(stmt1)
+
+def addstock(value):
+    sql2 = "select totalstock from stock"
+    stmt = ibm_db.prepare(conn, sql2)
+    result=ibm_db.execute(stmt)
+    movements=[]
+    row = ibm_db.fetch_assoc(stmt)
+    while(row):
+        movements.append(row)
+        row = ibm_db.fetch_assoc(stmt)
+    
+    oldvalue = movements[0]
+    newvalue = oldvalue + value
+    sql1="UPDATE stock SET totalstock = ?"
+        stmt1 = ibm_db.prepare(conn, sql1)
+        ibm_db.bind_param(stmt1,1,newvalue)
+        ibm_db.execute(stmt1)
+
+def removestock(value):
+    sql2 = "select totalstock from stock"
+    stmt = ibm_db.prepare(conn, sql2)
+    result=ibm_db.execute(stmt)
+    movements=[]
+    row = ibm_db.fetch_assoc(stmt)
+    while(row):
+        movements.append(row)
+        row = ibm_db.fetch_assoc(stmt)
+    
+    oldvalue = movements[0]
+    newvalue = oldvalue - value
+    sql1="UPDATE stock SET totalstock = ?"
+        stmt1 = ibm_db.prepare(conn, sql1)
+        ibm_db.bind_param(stmt1,1,newvalue)
+        ibm_db.execute(stmt1)
+
+def changethreshold(value):
+    sql1="update stockconfig set threshold = ?"
+        stmt1 = ibm_db.prepare(conn, sql1)
+        ibm_db.bind_param(stmt1,1,value)
+        ibm_db.execute(stmt1)
+
+@app.route('/modify')
+def modify():
+    threshold = request.form['username']
+    addstock = request.form['addstock']
+    removestock = request.form['removestock']
+    location = request.form['location']
+
+    if(threshold != 0):
+        changethreshold(threshold)
+    if(addstock != 0 ):
+        addstock(addstock)
+    if(removestock != 0):
+        removestock(removestock)
+    if(location != ''):
+        changelocation(location)
+if __name__ == '__main__':
+    app.run(debug=True)
